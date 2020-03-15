0f8669f6c366422f4cf9540387acdff99b222c88
Julio Salsavilca <j.salsavilca@gmail.com>
2020-03-15 14:11:06 -0500

    SEO all pages

--- App/app/Http/Controllers/Auth/LoginController.php
+++ App/app/Http/Controllers/Auth/LoginController.php
@@ -10,14 +10,45 @@ use Illuminate\Validation\Rule;
 class LoginController extends Controller
 {
 
+    protected $_meta = [];
+    protected $_metaTagSocials = [];
+    protected $_title = 'login';
+
     public function __construct()
     {
+        $this->_meta = [
+            'key' => 'description',
+            'value' => $this->description
+        ];
+
+        $this->setTitle( $this->_title );
+        $this->setMetaTags( $this->_meta );
+
         $this->middleware('guest')->except('logout');
     }
 
+    private function initialMetaTagSocial() {
+        $this->_metaTagSocials[] = [ 'social' => 'facebook', 'property' => 'fb:app_id', 'content' => env('FACEBOOK_APP_ID')];
+        $this->_metaTagSocials[] = [ 'social' => 'facebook', 'property' => 'og:url', 'content' => route('login.form')];
+        $this->_metaTagSocials[] = [ 'social' => 'facebook', 'property' => 'og:title', 'content' => $this->title . env( 'PROJECT_NAME' )];
+        $this->_metaTagSocials[] = [ 'social' => 'facebook', 'property' => 'og:description', 'content' => $this->description];
+        $this->_metaTagSocials[] = [ 'social' => 'facebook', 'property' => 'og:image', 'content' => asset( self::PATH_IMAGES . $this->logoSocial)];
+
+        $this->_metaTagSocials[] = [ 'social' => 'twitter', 'property' => 'twitter:url', 'content' => route('login.form')];
+        $this->_metaTagSocials[] = [ 'social' => 'twitter', 'property' => 'twitter:title', 'content' => $this->title . env( 'PROJECT_NAME' )];
+        $this->_metaTagSocials[] = [ 'social' => 'twitter', 'property' => 'twitter:description', 'content' => $this->description];
+        $this->_metaTagSocials[] = [ 'social' => 'twitter', 'property' => 'twitter:image', 'content' => asset( self::PATH_IMAGES . $this->logoSocial)];
+    }
+
     public function showLoginForm() {
+        $this->initialMetaTagSocial();
+        $this->setMetaTagSocial( $this->_metaTagSocials );
+
         $data = [
-            'activeSide' => 'login'
+            'activeSide' => 'login',
+            'title' => $this->title,
+            'metaTags' => $this->metaTags,
+            'metaTagSocials' => $this->metaTagSocials
         ];
         return view( 'classimax.pages.login', $data );
     }
